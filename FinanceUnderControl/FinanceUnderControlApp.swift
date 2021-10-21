cbd0d8d1cb481cb5bd1a447e903d229cba3bdd3c
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2021-10-21 17:37:48 +0200

    Initial Commit

---
+++ FinanceUnderControl/FinanceUnderControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceUnderControlApp.swift
+//  FinanceUnderControl
+//
+//  Created by Sebastian Staszczyk on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FinanceUnderControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
