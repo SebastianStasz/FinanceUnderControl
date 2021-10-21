40be615c4b0a7ca9520b1ba2c6cdf181b4419c15
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2021-10-21 19:10:10 +0200

    Initial setup

--- FinanceUnderControl/FinanceUnderControlApp.swift
+++ FinanceUnderControl/FinanceUnderControlApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FinanceUnderControlApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("Finance Under Control")
         }
     }
 }
