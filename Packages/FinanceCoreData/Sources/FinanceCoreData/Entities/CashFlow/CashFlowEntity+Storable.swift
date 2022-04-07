558c6c53ef84c9643976a0adf3009abdd311fb5c
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-04-07 19:14:53 +0200

    Cash flow entity + storable

---
+++ Packages/FinanceCoreData/Sources/FinanceCoreData/Entities/CashFlow/CashFlowEntity+Storable.swift
@@ -0,0 +1,29 @@
+//
+//  CashFlowEntity+Storable.swift
+//  FinanceCoreData
+//
+//  Created by sebastianstaszczyk on 06/04/2022.
+//
+
+import Foundation
+
+extension CashFlowEntity: Storable {
+    public typealias EntityDataModel = DataModel
+
+    public struct DataModel: Encodable {
+        let name: String
+        let date: Date
+        let value: Double
+        let categoryName: String
+        let currencyCode: String
+    }
+
+    public var dataModel: DataModel {
+        DataModel(name: name,
+                  date: date,
+                  value: value,
+                  categoryName: category.name,
+                  currencyCode: currency.code
+        )
+    }
+}
