308943df758299fdff60885c4914dd95353a94da
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-04-07 19:19:01 +0200

    Cash flow group entity + storable

---
+++ Packages/FinanceCoreData/Sources/FinanceCoreData/Entities/CashFlowCategoryGroup/CashFlowCategoryGroupEntity+Storable.swift
@@ -0,0 +1,21 @@
+//
+//  CashFlowCategoryGroupEntity+Storable.swift
+//  FinanceCoreData
+//
+//  Created by sebastianstaszczyk on 07/04/2022.
+//
+
+import Foundation
+
+extension CashFlowCategoryGroupEntity: Storable {
+    public typealias EntityDataModel = DataModel
+
+    public struct DataModel: Encodable {
+        let name: String
+        let type: CashFlowType
+    }
+
+    public var dataModel: DataModel {
+        DataModel(name: name, type: type)
+    }
+}
