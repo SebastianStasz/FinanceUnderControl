460954772393e5a9f882d9e121b9d6238efa40c5
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-05-05 15:51:52 +0200

    QueryDocumentSnapshot + get methods

---
+++ FinanceUnderControl/Firebase/QueryDocumentSnapshot+Get.swift
@@ -0,0 +1,37 @@
+//
+//  QueryDocumentSnapshot+Get.swift
+//  FinanceUnderControl
+//
+//  Created by sebastianstaszczyk on 05/05/2022.
+//
+
+import FirebaseFirestore
+import Foundation
+import Shared
+
+// TODO: Remove force unwrap and handle exceptions
+
+extension QueryDocumentSnapshot {
+    func get<T: DocumentField>(_ field: T) -> Any? {
+        get(field.rawValue)
+    }
+
+    func getString<T: DocumentField>(for field: T) -> String {
+        get(field.rawValue) as! String
+    }
+
+    func getCurrency<T: DocumentField>(for field: T) -> Currency {
+        let code = getString(for: field)
+        return Currency(rawValue: code)!
+    }
+
+    func getDecimal<T: DocumentField>(for field: T) -> Decimal {
+        let value = getString(for: field)
+        return try! Decimal(value, format: .number)
+    }
+
+    func getDate<T: DocumentField>(for field: T) -> Date {
+        let timestamp = get(field.rawValue) as! Timestamp
+        return timestamp.dateValue()
+    }
+}
